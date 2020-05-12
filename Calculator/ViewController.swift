49760d02e7de6cb08e4e3e48ced00d8083f6df65
Anh <anh@exerai.com>
2020-05-12 16:09:41 -0600

    Swift calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,83 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberDisplayLabel: UILabel?
+    var numberDisplayed: Double = 0
+    var previousNumber: Double = 0
+    var performingMath = false
+    var operation = 0
+
+    @IBAction func numbers(_ sender: UIButton) {
+        let tag = sender.tag - 1
+        if performingMath {
+            numberDisplayLabel?.text = String(tag)
+            if let unwrapped = numberDisplayLabel?.text {
+                numberDisplayed = Double(unwrapped)!
+            }
+            performingMath = false
+        } else {
+            guard let numberOnScreen = numberDisplayLabel?.text else { return }
+            numberDisplayLabel?.text = numberOnScreen + String(tag)
+            if let unwrapped = numberDisplayLabel?.text {
+                           numberDisplayed = Double(unwrapped)!
+                       }
+        }
+
+
+    }
+
+    @IBAction func buttons(_ sender: UIButton) {
+        let tag = sender.tag
+        if numberDisplayLabel?.text != "" && tag != 11 && tag != 16 {
+            if let unwrapped = numberDisplayLabel?.text {
+                           previousNumber = Double(unwrapped)!
+
+                        switch tag {
+                            case 12:
+                                print("divide")
+                                numberDisplayLabel?.text = "/"
+
+                            case 13:
+                                print("multiply")
+                                numberDisplayLabel?.text = "x"
+
+                            case 14:
+                                print("subtract")
+                                numberDisplayLabel?.text = "-"
+
+                            case 15:
+                                print("add")
+                                numberDisplayLabel?.text = "+"
+
+                            default:
+                                print("uhhh")
+                        }
+            }
+            operation = tag
+            performingMath = true
+        }
+        if tag == 16 {
+            switch operation {
+            case 12:
+                numberDisplayLabel?.text = "\(Double(previousNumber / numberDisplayed))"
+            case 13:
+                numberDisplayLabel?.text = "\(Double(previousNumber * numberDisplayed))"
+            case 14:
+                numberDisplayLabel?.text = "\(Double(previousNumber - numberDisplayed))"
+            case 15:
+                numberDisplayLabel?.text = "\(Double(previousNumber + numberDisplayed))"
+            default:
+                print("oh")
+            }
+        } else if tag == 11 {
+            numberDisplayLabel?.text = ""
+            previousNumber = 0
+            numberDisplayed = 0
+            operation = 0
+
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
